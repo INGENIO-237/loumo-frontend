452a62c3265f4e602f706a88f06e9b4199a5a2c1
INGENIO <ingeniokhalif@gmail.com>
2024-04-22 16:59:35 +0100

    updated dropdown-menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,41 +3,23 @@ import SearchBar from "./SearchBar";
 import MenuItem from "./MenuItem";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
-import { useDispatch } from "react-redux";
-import { toggleUserMenu } from "@/redux/slices/userDropdownSlice";
 import { Link } from "react-router-dom";
-import { useEffect, useRef } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 export default function Header() {
-  const showUserDropdownMenu = useSelector(
-    (state: RootState) => state.userDropdown.show
-  );
   const { isAuthenticated, user } = useSelector(
     (state: RootState) => state.auth
   );
-  const dispatch = useDispatch();
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
-      ) {
-        dispatch(toggleUserMenu(false));
-      }
-    };
-
-    document.addEventListener("mousedown", handleOutsideClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleOutsideClick);
-    };
-  }, []);
   return (
     <div className="w-full flex justify-evenly items-center bg-[#FEFEFE] p-5">
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-left pl-3">
         <Link to="/">
           <h1 className="text-3xl font-bold text-orange-500">Loumo</h1>
         </Link>
@@ -45,40 +27,42 @@ export default function Header() {
       <div className="w-full flex justify-center mx-2">
         <SearchBar />
       </div>
-      <div className="flex gap-x-4  w-full justify-center">
+      <div className="flex gap-x-4  w-full justify-end pr-3">
         <MenuItem icon={<Heart />} counter={0} link="#" />
         <MenuItem icon={<ShoppingCart />} counter={0} link="#" />
-        <div className="relative" ref={dropdownRef}>
-          <MenuItem
-            icon={
-              <User
-                onClick={() => dispatch(toggleUserMenu(!showUserDropdownMenu))}
-              />
-            }
-            link="#"
-          />
-          {showUserDropdownMenu ? (
-            isAuthenticated ? (
-              <div className="absolute shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in mt-5 bg-gray-200 p-2 rounded w-[180px]">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MenuItem icon={<User />} link="#" />
+          </DropdownMenuTrigger>
+          {isAuthenticated ? (
+            <DropdownMenuContent className="mr-3 bg-white">
+              <DropdownMenuItem>
                 <Link to="/profile">
                   <h1>Profile</h1>
                 </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
                 <Link to="#">
-                  <h1>wijdidwijdi</h1>
+                  <h1>hjhcjhuchsjoc</h1>
                 </Link>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
                 <Link to="#">
-                  <h1>wijdidwijdi</h1>
+                  <h1>Logout</h1>
                 </Link>
-              </div>
-            ) : (
-              <div className="absolute shadow-lg ring-1 ring-black ring-opacity-5 transition-all duration-300 ease-in mt-5 bg-gray-200 p-2 rounded w-[180px]">
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          ) : (
+            <DropdownMenuContent className="mr-3 bg-white">
+              <DropdownMenuItem>
                 <Link to="/login">
                   <h1>Login</h1>
                 </Link>
-              </div>
-            )
-          ) : null}
-        </div>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          )}
+        </DropdownMenu>
       </div>
     </div>
   );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import userDropdownReducer from "./slices/userDropdownSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    userDropdown: userDropdownReducer,
   },
 });
 
