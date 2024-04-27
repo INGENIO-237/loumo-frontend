58b65712a3bc5334eca9ff355cd1592b7fe1dbbf
INGENIO <ingeniokhalif@gmail.com>
2024-04-27 10:23:42 +0100

    added logic to persist redux state

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.ts";
+import { persistor, store } from "./redux/store.ts";
 import Routes from "./Routes.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,13 +20,15 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Router>
-          <Routes />
-        </Router>
-        <ToastContainer hideProgressBar stacked position="bottom-right" />
-      </Provider>
-    </QueryClientProvider>
+    <PersistGate persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Router>
+            <Routes />
+          </Router>
+          <ToastContainer hideProgressBar stacked position="bottom-right" />
+        </Provider>
+      </QueryClientProvider>
+    </PersistGate>
   </React.StrictMode>
 );
